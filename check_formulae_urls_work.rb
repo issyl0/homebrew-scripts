a8f5e9ebcf3ef2078916e38fe8fb88233f722bb7
Isabell (Issy) Long <me@issyl0.co.uk>
2019-05-04 00:41:24 +0100

    Add a script to check that the `url` values in formulae work
    - This loops through every formula in the homebrew-core/Formula
      directory (locally) and prints the status code of accessing the
      found URL.
    - There will be another script for other stanzas, like `mirror`s or
      `homepage`s.

---
+++ check_formulae_urls_work.rb
@@ -0,0 +1,36 @@
+require 'rest-client'
+
+def get_url(formula)
+  File.open(formula).grep(/url/).first.split(' ')[1].delete('",')
+end
+
+formulae_directory = '/Users/issyl0/repos/osc/dev-homebrew/core/Formula'
+
+Dir.glob("#{formulae_directory}/*.rb").each do |formula|
+  next if %w[. ..].include?(formula)
+
+  url = get_url(formula)
+  formula_name = File.basename(formula, File.extname(formula)).split('/').last
+
+  begin
+    RestClient.get(url) do |response|
+      puts "#{formula_name}: #{response.code} for #{url}"
+    end
+  rescue RestClient::SSLCertificateNotVerified
+    puts "#{formula_name}: SSL errors for #{url}"
+    next
+  rescue RestClient::Exceptions::OpenTimeout
+    puts "#{formula_name}: timeout for #{url}"
+    next
+  rescue SocketError
+    puts "#{formula_name}: socket error for #{url}"
+    next
+  rescue ArgumentError
+    # TODO: This happens if the `url` stanza is commented out.
+    puts "#{formula_name}: argument error for #{url}"
+    next
+  rescue Errno::ECONNREFUSED
+    puts "#{formula_name}: connection refused for #{url}"
+    next
+  end
+end
