401af3c5a259a432e77b9d3c054b02a1b96a37c2
Isabell (Issy) Long <me@issyl0.co.uk>
2019-05-05 15:39:34 +0100

    Add a script to check that the `url` values in formulae work
    - `brew audit --strict --online` also does this, but requires Homebrew
      installed. This just requires a checkout of the repo.
    - Use the FORMULAE_DIR envvar to specify the repo's location.

--- check_formulae_urls_work.rb
+++ check_formulae_urls_work.rb
@@ -6,33 +6,36 @@ end
 
 formulae_directory = ENV['FORMULAE_DIR']
 
-puts "Running checks against formulae in #{formulae_directory}."
+if !Dir.glob("#{formulae_directory}/*.rb").empty?
+  Dir.glob("#{formulae_directory}/*.rb").each do |formula|
+    next if %w[. ..].include?(formula)
 
-Dir.glob("#{formulae_directory}/*.rb").each do |formula|
-  next if %w[. ..].include?(formula)
+    url = get_url(formula)
+    formula_name = File.basename(formula, File.extname(formula)).split('/').last
 
-  url = get_url(formula)
-  formula_name = File.basename(formula, File.extname(formula)).split('/').last
-
-  begin
-    RestClient.get(url) do |response|
-      puts "#{formula_name}: #{response.code} for #{url}"
+    begin
+      RestClient.get(url) do |response|
+        puts "#{formula_name}: #{response.code} for #{url}"
+      end
+    rescue RestClient::SSLCertificateNotVerified
+      puts "#{formula_name}: SSL errors for #{url}"
+      next
+    rescue RestClient::Exceptions::OpenTimeout
+      puts "#{formula_name}: timeout for #{url}"
+      next
+    rescue SocketError
+      puts "#{formula_name}: socket error for #{url}"
+      next
+    rescue ArgumentError
+      # TODO: This happens if the `url` stanza is commented out.
+      puts "#{formula_name}: argument error for #{url}"
+      next
+    rescue Errno::ECONNREFUSED
+      puts "#{formula_name}: connection refused for #{url}"
+      next
     end
-  rescue RestClient::SSLCertificateNotVerified
-    puts "#{formula_name}: SSL errors for #{url}"
-    next
-  rescue RestClient::Exceptions::OpenTimeout
-    puts "#{formula_name}: timeout for #{url}"
-    next
-  rescue SocketError
-    puts "#{formula_name}: socket error for #{url}"
-    next
-  rescue ArgumentError
-    # TODO: This happens if the `url` stanza is commented out.
-    puts "#{formula_name}: argument error for #{url}"
-    next
-  rescue Errno::ECONNREFUSED
-    puts "#{formula_name}: connection refused for #{url}"
-    next
   end
+else
+  puts "Couldn't find any Ruby files in #{formulae_directory}. Aborting."
+  exit(1)
 end
