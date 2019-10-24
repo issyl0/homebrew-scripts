8cd2290b7466410606bcab8b39481dd2b96572de
Issy Long <me@issyl0.co.uk>
2019-10-24 21:55:11 +0100

    wip

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/bionic64"
+  config.vm.hostname = "microscopium"
+
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ['modifyvm', :id, '--usb', 'on']
+    # This is my red Yubikey.
+    vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'SmartCard', '--vendorid', '0x1050', '--productid', '0x0407']
+  end
+
+ config.vm.provision :shell, path: "homebrew-linux-maintainer-setup.sh"
+end
---
+++ homebrew-linux-maintainer-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo useradd issyl0
+sudo usermod -aG issyl0
+sudo su - issyl0
+
+sudo apt-get install build-essential curl file git
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+mkdir ~/repos
+git clone https://github.com/issyl0/dotfiles ~/repos/dotfiles
+ln -s ~/repos/dotfiles/.vimrc ~/.vimrc
+ln -s ~/repos/dotfiles/.zshrc ~/.zshrc
+ln -s ~/repos/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/repos/dotfiles/.gitignore_global ~/.gitignore_global
+
+brew update
+brew bundle --file=~/repos/dotfiles/Brewfile
+
+# Make Yubikey work.
+gpg --recv-keys 0x8247C390DADC67D4
+mkdir ~/.gnupg
+ln -s ~/repos/dotfiles/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+ln -s ~/repos/dotfiles/gnupg.conf ~/.gnupg/gpg.conf
+
+echo "All done? Maybe?"
